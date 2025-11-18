937d52ace91309327708128920a6c9f82921755b
paraZmol <nunes.20.24@gmail.com>
2025-11-18 12:16:48 -0500

    feat: Crear la logica que genra el pdf

--- app/Http/Controllers/PrestamoPdfController.php
+++ app/Http/Controllers/PrestamoPdfController.php
@@ -2,9 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Prestamo;
+use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Http\Request;
 
 class PrestamoPdfController extends Controller
 {
-    //
+    public function imprimir(Prestamo $prestamo)
+    {
+        // datos relacionados ale prestamo
+        $prestamo->load(['estudiante.escuela', 'item.tablet', 'item.tesis']);
+
+        //crear el pdf
+        $pdf = Pdf::loadView('pdf.boleta', ['prestamo'=> $prestamo]);
+
+        // tamaño de papael en A4
+        $pdf->setPaper('a4', 'portrait');
+
+        // para abrir una nueva pestaña con pdf
+        return $pdf->stream("boleta-prestamo-{$prestamo->id}.pdf");
+    }
 }
