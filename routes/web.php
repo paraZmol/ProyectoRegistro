15df1716cf798a6c34126fa1a2b19d699965a3a4
paraZmol <nunes.20.24@gmail.com>
2025-11-18 12:03:09 -0500

    feat:Agregar la ruta en el web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PrestamoPdfController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/prestamos/{prestamo}/boleta', [PrestamoPdfController::class, 'imprimir'])
+    ->name('prestamos.boleta');
