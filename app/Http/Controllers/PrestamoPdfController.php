259511f45abaca70f34b33775a3315dcc2d1efcd
paraZmol <nunes.20.24@gmail.com>
2025-11-19 21:20:18 -0500

    feat: Agregar rutas y funcion al controller

--- app/Http/Controllers/PrestamoPdfController.php
+++ app/Http/Controllers/PrestamoPdfController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 
 class PrestamoPdfController extends Controller
 {
+    // boleta individual
     public function imprimir(Prestamo $prestamo)
     {
         // datos relacionados ale prestamo
@@ -27,4 +28,73 @@ class PrestamoPdfController extends Controller
         return $pdf->stream("boleta-prestamo-{$prestamo->id}.pdf");
         //return view('pdf.boleta', ['prestamo' => $prestamo]);
     }
+
+    // boleta en lista
+    public function imprimirListado(Request $request)
+    {
+        // obtener filtros aplicados desde la URL
+        $filtros = $request->all();
+
+        // para construir la query base
+        $query = Prestamo::query()
+            ->with(['estudiante.escuela', 'item.tablet', 'item.tesis']);
+
+        // filtro 1 - prestamos activos por defecto
+        if ($request->has('tableFilters.prestamos_activos.value')) {
+            $value = $request->input('tableFilters.prestamos_activos.value');
+
+            if ($value) {
+                $query->whereNull('momento_entrega');
+            }
+        }
+
+        // filtro 2 - estudiante
+        if ($request->has('tableFilters.estudiante_id.value')) {
+            $query->where('estudiante_id', $request->input('tableFilters.estudiante_id.value'));
+        }
+
+        // filtro 3 - carnet
+        if ($request->has('tableFilters.carnet.value')) {
+            $query->whereHas('estudiante', function ($q) use ($request) {
+                $q->where('carnet', 'like', '%' . $request->input('tableFilters.carnet.value') . '%');
+            });
+        }
+
+        // filtro 4 → rango de fechas
+        if ($request->has('tableFilters.rango_fechas.value')) {
+            $tipo = $request->input('tableFilters.rango_fechas.value.tipo_fecha');
+            $desde = $request->input('tableFilters.rango_fechas.value.desde');
+            $hasta = $request->input('tableFilters.rango_fechas.value.hasta');
+
+            if ($tipo) {
+                $campo = $tipo === 'prestamo' ? 'momento_prestamo' : 'momento_entrega';
+
+                if ($desde) {
+                    $query->whereDate($campo, '>=', $desde);
+                }
+                if ($hasta) {
+                    $query->whereDate($campo, '<=', $hasta);
+                }
+            }
+        }
+
+        // obtener resultados filtrados
+        $prestamos = $query->orderBy('created_at', 'desc')->get();
+
+        // generar PDF
+        $pdf = Pdf::loadView('pdf.prestamos-listado', [
+            'prestamos' => $prestamos,
+            'filtros' => $filtros,
+        ]);
+
+        $pdf->setOption([
+            'isRemoteEnabled' => true,
+            'isHtml5ParserEnabled' => true,
+        ]);
+
+        $pdf->setPaper('a4', 'portrait');
+
+        return $pdf->stream("prestamos-filtrados.pdf");
+    }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -6,5 +6,9 @@ use App\Http\Controllers\PrestamoPdfController;
 Route::get('/', function () {
     return view('welcome');
 });
+// prestamo individual
 Route::get('/prestamos/{prestamo}/boleta', [PrestamoPdfController::class, 'imprimir'])
     ->name('prestamos.boleta');
+// prestamo en lista
+Route::get('/prestamos/imprimir', [PrestamoPdfController::class, 'imprimirListado'])
+    ->name('prestamos.imprimir');
